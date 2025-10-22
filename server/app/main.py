ef035e71a5a4e07b4523c61e8facabf08ecaf401
Pranath Prasanth <pranathp05@gmail.com>
2025-10-22 23:28:33 +0530

    5commit

--- server/app/main.py
+++ server/app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
-from app.database import create_tables, seed_demo_data
-from app.routers import (
+from .database import create_tables, seed_demo_data
+from .routers import (
     notifications,
     disasters,
     camps,
